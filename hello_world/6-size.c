503f587e5fbfedb217cc904fafe64304c3919f15
root <queisecarvalho@hotmail.com>
2023-02-17 05:30:33 -0800

    C task 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main  - Program C
+* Return 0
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
