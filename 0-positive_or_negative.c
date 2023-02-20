9e6cca79e9832a76bbf2801b56f1b63b880ab55f
root <queisecarvalho@hotmail.com>
2023-02-20 08:45:38 -0800

    if else task 0

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ *  * main - print whether thtored in the variable n is positive or negative number stored in the variable n is positive or negative
+ *   *
+ *    * Return 0 (Sucess)
+ *     */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	        if (n > 0) {
+			        printf("%d is positive \n", n);
+				        }
+		        else if (n == 0) {
+				        printf("%d is zero \n", n);
+					        }
+			        else {
+					        printf("%d is negativo \n", n);
+						        }
+				return (0);
+}
