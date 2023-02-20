8eed9f52bf087b713d3a44ba2ffefeab13b06244
root <queisecarvalho@hotmail.com>
2023-02-20 09:13:02 -0800

    if else task 0

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,14 +16,18 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 
 sitive_or_negative.c
-if (n > 0) {
-printf("%d is positive \n", n)	
+
+if (n > 0)
+{
+printf("%d is positive \n", n);	
 }
-else if (n == 0) {
+else if (n == 0)
+{
 printf("%d is zero \n", n);
 }
-else {
-printf("%d is negativo \n", n);
+else 
+{
+printf("%d is negative \n", n);
 }
 return (0);
 }
