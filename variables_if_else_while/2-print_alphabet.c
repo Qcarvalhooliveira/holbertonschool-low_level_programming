73dffe1e09b3b792cebf127018255c26bb2ce33e
root <queisecarvalho@hotmail.com>
2023-02-20 10:21:15 -0800

    if else task 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints positive or negative numbers
+ * Return: 0 (Sucess)
+ *
+ */
+
+int main(void)
+
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
