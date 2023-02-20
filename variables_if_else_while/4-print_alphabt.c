0b7ae568a50e9de5cbd3c51ce0724bcafca61b92
root <queisecarvalho@hotmail.com>
2023-02-20 12:11:34 -0800

    if else task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints single digit numbers
+ *
+ * Return: 0 (Sucess)
+ */
+
+int main(void)
+{
+
+	         int num;
+
+		 for (num = 0; num < 10; num < 10; num ++)
+			 printf("%d", num);
+				           
+		 printf ('\n')
+	
+	         return (0);
+
+}
