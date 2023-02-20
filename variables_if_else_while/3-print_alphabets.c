cc1a980450f85d9bbea6b4376b3ab38de68fa2f4
root <queisecarvalho@hotmail.com>
2023-02-20 11:42:15 -0800

    if else task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -5,23 +5,21 @@
 /**
  * main - prints alphabet lowcase and uppercase
  * Return: 0 (Sucess)
- *
  */
 
 int main(void)
 
 {
 
-char low;
+         char low;
 
-for (low = 'a'; low <= 'z'; low++){
-    putchar(low);
-}
-for (low = 'A'; low = 'Z'; low++){
-    putchar(low);
+         for (low = 'a'; low <= 'z'; low++)
+         putchar(low);
 
-putchar('\n');
-}
-return (0);
+         for (low = 'A'; low = 'Z'; low++)
+         putchar(low);
+         putchar('\n');
+
+         return (0);
 
 }
