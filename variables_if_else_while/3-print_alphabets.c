e36793ccf01f9479d2a675d29270404039098341
root <queisecarvalho@hotmail.com>
2023-02-20 11:01:01 -0800

    if else task 3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -11,14 +11,16 @@
 int main(void)
 
 {
-	char low;
 
-	for(low = 'a'; low <= 'z'; low++)
-	putchar(low);
+char low;
 
-	for(low = 'A'; low = 'Z'; low++)
-	putchar(low);
-	putchar('\n');
+for(low = 'a'; low <= 'z'; low++)
+putchar(low);
+
+for(low = 'A'; low = 'Z'; low++)
+putchar(low);
+putchar('\n');
 	
-	return (0);
+return (0);
+
 }
