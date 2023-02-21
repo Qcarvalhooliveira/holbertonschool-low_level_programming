2b55fdf399b305d38c72de0b3acbf5364220bca7
root <queisecarvalho@hotmail.com>
2023-02-21 07:48:30 -0800

    Functions task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
  * main - prints _putchar
@@ -9,9 +7,20 @@
  *
  */
 
-int _putchar(char c)
+int main(void)
 
 {
-				       
-	return (write(1, &c, 1));
+
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+      	
+return (0);
+
 }
