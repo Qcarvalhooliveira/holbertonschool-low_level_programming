59fcadb2eb675badbd45dbdbb69eb8b5155f909e
root <queisecarvalho@hotmail.com>
2023-02-21 01:38:20 -0800

    if eslse task 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabet in lowercase
+ *
+ * Return: 0 (Sucess)
+ *
+ */
+
+int main(void)
+
+{
+
+int i;
+
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf('\n');
+
+return (0);
+
+}
