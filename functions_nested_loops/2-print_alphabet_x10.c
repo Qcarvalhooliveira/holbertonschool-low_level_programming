e1f87205eff1f51623ef047502a55ad426656acf
root <queisecarvalho@hotmail.com>
2023-02-21 10:17:21 -0800

    Fonctions task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -11,17 +11,17 @@ void print_alphabet_x10(void)
 
 {
 
-           char c;
-           int i = 0;
+char c;
+int i = 0;
 
-           while (i <= 9)
-           {
-           for (c = 'a'; c <= 'z'; c++)
+while (i <= 9)
+{
+for (c = 'a'; c <= 'z'; c++)
 
-           _putchar(c);
+_putchar(c);
 
-           _putchar('\n');
-           i++;
-           }
+_putchar('\n');
+i++;
+}
 
 }
