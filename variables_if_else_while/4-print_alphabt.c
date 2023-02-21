3161eb05d122189a625147d1ae2c3cf6e2cb6de6
root <queisecarvalho@hotmail.com>
2023-02-21 01:14:36 -0800

    if eslse task 4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,17 +6,19 @@
  * main - prints single digit numbers
  *
  * Return: 0 (Sucess)
+ *
  */
 
 int main(void)
 {
+
 int num;
 
 for (num = 0; num < 10; num ++)
 printf("%d", num);
-				           
+
 printf ('\n');
-	
+
 return (0);
 
 }
