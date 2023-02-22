d90ac9d410331a16d39535633d472de6c5c09692
root <queisecarvalho@hotmail.com>
2023-02-22 01:26:46 -0800

    Functions task 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase character
+ *
+ * Return: 1 for lowercase character or 0 for anything else
+ *
+ */
+
+int _islower(int c);
+
+{
+
+if (c >= 97 && c <= 122)
+{
+
+return (1);
+}
+return(0);
+}
