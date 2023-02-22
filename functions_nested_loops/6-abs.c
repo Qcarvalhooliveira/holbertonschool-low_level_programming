3cc58429dbbf2871dc78da4e1bf54a7de58acd7f
root <queisecarvalho@hotmail.com>
2023-02-22 02:55:22 -0800

    Functions task 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ *
+ * @n: The character to be checked.
+ *
+ * Return: Absolute value of number or zero.
+ *
+ */
+
+int _abs(int)
+
+{
+
+if (c > 0)
+{
+int abs_val;
+
+abs_val = c * -1;
+return (abs_val);
+}
+return (c);
+}
