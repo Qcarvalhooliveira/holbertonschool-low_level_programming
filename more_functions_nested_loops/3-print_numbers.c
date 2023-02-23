623d73281e6a5e759d554f560766274afccfa961
root <queisecarvalho@hotmail.com>
2023-02-23 12:20:11 -0800

    More functions task 3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_numbers - prints the numbers, from 0 to 9.
+ * print_numbers - Prints the numbers, from 0 to 9.
  *
  */
 
@@ -10,10 +10,11 @@ void print_numbers(void)
 
 {
 
-     
 int i;
 
 for (i = 0; i < 10; i++)
+
 _putchar(i + '0');
+
 _putchar('\n');
 }
