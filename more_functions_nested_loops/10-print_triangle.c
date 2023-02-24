9ba4de206a99aa78a16e19c8492f4e7253a15698
root <queisecarvalho@hotmail.com>
2023-02-24 03:25:51 -0800

    More fuctions task 10

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -15,12 +15,13 @@ int i, j, k;
 
 if (size <= 0)
 putchar('\n');
+
 for (i = 0; i < size; i++)
 {
 for (j = size - 1; j > i; j--)
 {
 putchar(' ');
-}
+{
 for (k = 0; k <= i; k++)
 }
 putchar('#');
