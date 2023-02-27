af1decb76f9f7757b8eeddb40981278fa0841218
root <queisecarvalho@hotmail.com>
2023-02-27 02:22:47 -0800

    Pointers task 0

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
 /**
  * reset_to_98 - Takes a pointer to an int. 
  *
- * Return: Always 0.
+ * @n: Pointer *n being initialised.
  *
  */
 
@@ -12,13 +12,6 @@ void reset_to_98(int *n)
 
 {
        
-int n;
-
-n = 98;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-
-return (0);
+*n = 98;
 
 }
