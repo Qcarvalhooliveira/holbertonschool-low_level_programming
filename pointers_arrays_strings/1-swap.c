160bca2494fed4a40c3285f3077343c4110e6cfd
root <queisecarvalho@hotmail.com>
2023-02-27 03:59:52 -0800

    Pointers task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - Swaps the values of two integers.
+ *@a: First integer being swapped.
+ *@b: Second integer being swapped.
+ */
+
+void swap_int(int *a, int *b)
+
+{
+
+int tmp = *a;
+*a = *b;
+*b = tmp;
+
+}
