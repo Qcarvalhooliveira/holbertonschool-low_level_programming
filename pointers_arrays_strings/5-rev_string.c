7b150ad9e9ac27e8ab7af2c1a79edc307b0c9365
root <queisecarvalho@hotmail.com>
2023-02-28 01:54:21 -0800

    Pointers task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Returns the length of a string.
+ *@s: String being evaluated.
+ *
+ * Return: Length of string
+ */
+
+int _strlen(char *s)
+
+{
+
+int i = 0;
+while (*s++)
+i++;
+return (i);
+
+}
+
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string.
+ * @s: Array being reversed.
+ *    */
+
+void rev_string(char *s)
+
+{
+int len, i, tmp;
+
+len = _strlen(s) - 1;
+for (i = 0; i < (len / 2); i++)
+{
+tmp = s[i];
+s[i] = s[len -i];
+s[len -i] = tmp;
+}
+
+}
