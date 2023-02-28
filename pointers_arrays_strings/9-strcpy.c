d87531d85dc4d414f3867ea5f93889911e42a225
root <queisecarvalho@hotmail.com>
2023-02-28 06:37:40 -0800

    Pointers task 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * char *_strcpy - Function that copies the string pointed to by src.
+ * @dest: copy to
+ * @src: copy from
+ * Return: string.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int l = 0;
+int x = 0;
+
+while (*(src + 1) != '\0')
+{
+l++;
+}
+for ( ; x < 1; x++)
+{
+dest[x] = src[x];
+}
+dest[l] = '\0';
+return (dest);
+}
