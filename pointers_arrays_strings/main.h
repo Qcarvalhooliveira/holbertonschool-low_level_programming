2479825acddea25315dfb175c0ba0342eef28fba
root <queisecarvalho@hotmail.com>
2023-03-01 01:48:40 -0800

    Arrays task 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,21 +1,15 @@
-void reset_to_98(int *n);
+char *_strcat(char *dest, char *src);
 
-void swap_int(int *a, int *b);
+char *_strncat(char *dest, char *src, int n);
 
-int _strlen(char *s);
+char *_strncpy(char *dest, char *src, int n);
 
-void _puts(char *str);
+int _strcmp(char *s1, char *s2);
 
-void print_rev(char *s);
+void reverse_array(int *a, int n);
 
-void rev_string(char *s);
+char *string_toupper(char *);
 
-void puts2(char *str);
+char *cap_string(char *);
 
-void puts_half(char *str);
 
-void print_array(int *a, int n);
-
-char *_strcpy(char *dest, char *src);
-
-int _atoi(char *s);
