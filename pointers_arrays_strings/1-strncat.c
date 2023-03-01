56658cae4d8b8c7aaaa0a15c0a65b6bcfb9130f5
root <queisecarvalho@hotmail.com>
2023-03-01 03:59:57 -0800

    Arrays task 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcat - Function that concatenates two strings.
+ * @dest: string being concatenate
+ * @src: string being concatenate
+ * @n: Number of bytes.
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+int i = 0;
+int j = 0;
+
+while (dest[i])
+i++;
+while (j < n && src[j] != '\0')
+dest[i++] = src[j++];
+dest [i] = '\0';
+return (dest);
+}
