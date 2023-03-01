8bd07a190a417cc8f9bc479335587b8135ad5516
root <queisecarvalho@hotmail.com>
2023-03-01 06:06:39 -0800

    Arrays task 3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcmp - Function that compares two strings.
+ * @s1: string being compared.
+ * @s2: string being compared.
+ *
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+
+
+}
+
+int i;
+
+i = 0;
+
+while (s1[i] && s2[i] && s1[i] == s2[i])
+i++;
+
+return (0);
+}
+
