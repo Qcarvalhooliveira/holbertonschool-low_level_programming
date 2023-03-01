c68b7afe5db8eaf47b02afb6ebaa592ad4b0edef
root <queisecarvalho@hotmail.com>
2023-03-01 04:37:17 -0800

    Arrays task 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -16,7 +16,7 @@ char *_strncpy(char *dest, char *src, int n)
 int i = 0;
 int j = 0;
 
-for (i = 0; < dest[i] && src[i] != '\0'; i++)
+for (i = 0; dest[i] && src[i] != '\0'; i++)
 dest[i++] = src[j++];
 dest [i] = '\0';
 return (dest);
