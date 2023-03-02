caaa1786efad9c36897f29b481227df5185c8346
root <queisecarvalho@hotmail.com>
2023-03-02 12:30:35 -0800

    string task 0

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,15 +1,17 @@
-char *_strcat(char *dest, char *src);
+char *_memset(char *s, char b, unsigned int n);
 
-char *_strncat(char *dest, char *src, int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
-char *_strncpy(char *dest, char *src, int n);
+char *_strchr(char *s, char c);
 
-int _strcmp(char *s1, char *s2);
+unsigned int _strspn(char *s, char *accept);
 
-void reverse_array(int *a, int n);
+char *_strpbrk(char *s, char *accept);
 
-char *string_toupper(char *);
+char *_strstr(char *haystack, char *needle);
 
-char *cap_string(char *);
+void print_chessboard(char (*a)[8]);
+
+void print_diagsums(int *a, int size);
 
 
