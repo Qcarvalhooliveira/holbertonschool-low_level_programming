9366966b446210da04a5c8c1c5e0463b42f49b67
root <queisecarvalho@hotmail.com>
2023-03-02 13:13:54 -0800

    string task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * _srtchr - Function that locates a character in a string..
- * @s: string being evaluated..
- * @c: character being checked..
+ * *_srtchr - Function that locates a character in a string.
+ * @s: string being evaluated.
+ * @c: character being checked.
  *
  * Return: pointer to first occurence of character.
  */
@@ -13,10 +13,10 @@ char *_strchr(char *s, char c)
 {
 int i = 0;
 
-while ( s[i++])
-{	
+while (s[i++])
+{
 if (s[i] == c)
-{	
+{
 return (s + i);
 }
 }
