5aaa2362f7b1d7d4b92ece16667dc8257eefb8a7
root <queisecarvalho@hotmail.com>
2023-03-02 05:04:42 -0800

    Arrays task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -15,8 +15,8 @@ char letter[] = "AaeEoOtTLl";
 char number[] = "4433007711";
 
 for (i = 0; n[i]; i++)
-if (j = 0; letter[j]; j++);
-if (letter[j] == n[i]);
+if (j = 0; letter[j]; j++)
+if (letter[j] == n[i])
 s[i] == number[j];
 return (n);
 }
