b8b11d03ab5baa010cb802e6a02839311746a91d
root <queisecarvalho@hotmail.com>
2023-03-02 12:36:38 -0800

    string task 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -6,7 +6,7 @@
  * @s: starting address of memory to be filled.
  * @b: the desired value.
  * @n: number of bytes to be changed.
- * 
+ *
  * Return: changed array with new value for n bytes.
  */
 
