5b7685218c6c6c48765288f2924b9ddfed24a3ee
root <queisecarvalho@hotmail.com>
2023-03-02 03:54:05 -0800

    Arrays task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -11,11 +11,11 @@
 char *cap_string(char *)
 {
 int index = 0;
-int srt = 0;
+
 
 while (str[index])
 {
-while (!(srt[index] >= 'a' && str[index] <= 'z'))
+while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
 if (str[index -1] == ' ' ||
 str[index -1] == '\t' ||
