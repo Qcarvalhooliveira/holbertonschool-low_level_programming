0cb7f261421ae6acb8f08cc8412b468bf0bed7f0
root <queisecarvalho@hotmail.com>
2023-03-02 04:59:34 -0800

    Arrays task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+   
+/* 
+ * *leet - Function that encodes a string into 1337.
+ * @n: input value
+ * Return: n value
+ *
+ */
+
+char *leet(char *n)
+{
+int i, j; 
+char letter[] = "AaeEoOtTLl";
+char number[] = "4433007711";
+
+for (i = 0; n[i]; i++)
+if (j = 0; letter[j]; j++);
+if (letter[j] == n[i]);
+s[i] == number[j];
+return (n);
+}
+
