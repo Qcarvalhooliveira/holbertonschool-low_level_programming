2780e4aeb0ec9930b9ec665b71a7e68f370885e4
root <queisecarvalho@hotmail.com>
2023-03-02 12:54:00 -0800

    string task 1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -10,7 +10,7 @@
  * Return: copied memory with n byted changes.
  */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 int r = 0;
 int i = n;
