c15ca049b8ee69194f635647b71b82500acc0c09
root <queisecarvalho@hotmail.com>
2023-03-03 14:13:39 -0800

    Arrays task 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -14,7 +14,7 @@ int j = size - 1;
 int sul1 = 0;
 int sum2 = 0;
 
-for (i = 0; i < size * size; i = i size + 1, j = j + size - 1)
+for (i = 0; i < size * size; i = i + size + 1, j = j + size - 1)
 {
 sum1 = sum1 + a[i];
 sum2 = sum2 + a[j];
