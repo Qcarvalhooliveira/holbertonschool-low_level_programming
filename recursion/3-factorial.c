5964e3764aca23797899f395ed59223cf03512b3
root <queisecarvalho@hotmail.com>
2023-03-06 05:26:16 -0800

    Recursion task 3

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -5,7 +5,7 @@
 /**
  * factorial - Function that returns the factorial of a given number..
  *
- * @n: string being evaluated.
+ * @n: number being evaluated.
  * Return: If n is lower than 0, the function should return -1.
  */
 
@@ -13,12 +13,10 @@ int factorial(int n)
 {
 
 if(n < 0)
-{
 return -1;
 else if (!n)
 return (1);
-}
-   return (n * factorial(n - 1));
+return (n * factorial(n - 1));
 }
 
 
