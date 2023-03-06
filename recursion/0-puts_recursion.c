1d4a7a36b67c192fde5216fc0761329434060c33
root <queisecarvalho@hotmail.com>
2023-03-06 03:13:58 -0800

    Recursion task 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts_reccursion - Function that prints a string.
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+
+char s = 0;
+{
+if (s = 0; s <= 0; s++)
+
+return (0);
+}
