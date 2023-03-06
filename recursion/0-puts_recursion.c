17446a8671b3224ee402f27069f51cda0fdbb5d4
root <queisecarvalho@hotmail.com>
2023-03-06 03:31:34 -0800

    Recursion task 0

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,13 +8,13 @@
  */
 
 void _puts_recursion(char *s)
-{
+	{
 
-if (!*s)
-{
-putchar('\n');
-return;
-}
-putchar(*s);
-puts_recursion(s + 1);
-}
+	if (!*s)
+	{
+	_putchar('\n');
+	return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
