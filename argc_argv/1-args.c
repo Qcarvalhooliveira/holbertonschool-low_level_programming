e7cf61f4c546c8c8708dd1556def392f83cbb27e
root <queisecarvalho@hotmail.com>
2023-03-08 04:56:15 -0800

    argc task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints the number of arguments.
+ *
+ * @argc: number of the command line arguments.
+ * @argv: array containing command line arguments.
+ *
+ * Return: 0.
+ *
+ */
+
+int main(int argc,  char *argv[]  __attribute__((unused)))
+{
+
+printf("%i\n", argc -1);
+return (0);
+}
