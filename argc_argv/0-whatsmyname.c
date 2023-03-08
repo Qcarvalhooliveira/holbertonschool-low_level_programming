e8238c73d5d32d22384d0776432ec19a3aa5b99d
root <queisecarvalho@hotmail.com>
2023-03-08 04:50:06 -0800

    argc task 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Program that prints its name, followed by a new line.
+ *
+ * @argc: number of the command line arguments.
+ * @argv: array containing command line arguments.
+ *
+ * Return: 0.
+ *
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+printf("%s\n", argv[0]);
+return (0);
+}
