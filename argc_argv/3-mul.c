98f2f86c1e8bc11cc1db950fa40dd45299fe77b5
root <queisecarvalho@hotmail.com>
2023-03-10 01:19:59 -0800

    argc task 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that multiplies two numbers..
+ *
+ * @argc: number of the command line arguments.
+ * @argv: array containing command line arguments.
+ *
+ * Return: 0, 1 if more than 2 arguments
+ *
+ */
+
+int main(int argc,  char *argv[])
+
+{
+
+if (argc == 3)
+{
+printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
+}
+else
+{
+printf("Error\n");
+}
+return (1);
+}
