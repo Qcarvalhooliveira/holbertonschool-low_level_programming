6ce44c567c984882f865b5edac0d59f2e9b7658c
root <queisecarvalho@hotmail.com>
2023-03-10 01:39:43 -0800

    argc task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that adds positive numbers.
+ *
+ * @argc: number of the command line arguments.
+ * @argv: array containing command line arguments.
+ *
+ * Return: 0, 1 if non-digits are passed.
+ *
+ */
+
+int main(int argc,  char *argv[])
+
+{
+int i;
+int sum = 0;
+char *endptr;
+
+if (argc > 1)
+{
+for (i = 1; i < argc; ++i)
+{
+sum += strtol(argv[i], &endptr, 10);
+if ((*endptr >= 'A' && *endptr <= 'Z')
+|| (*endptr >= 'a' && *endptr <= 'z'))
+{
+printf("Error\n");
+return (1);
+}
+}
+printf("0\n");
+}
+return (0);
+}
