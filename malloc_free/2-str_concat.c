37ec941ddca8b5ced16c76c9da69f04d486e0fa7
root <queisecarvalho@hotmail.com>
2023-03-14 12:25:50 -0700

    Malloc task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,22 +1,41 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * *str_concat - Function that concatenates two strings.
- * @s1: String to be concatened.
- * @s2: String to be concatened.
+ * str_concat - Function that concatenates two strings.
+ * @s1: String to be concatenated.
+ * @s2: String to be concatenated.
  *
- * Return: Pointer to new string , NULL if str = NULL
+ * Return: Concatenated string , NULL if empty or fail.
  */
 
 char *str_concat(char *s1, char *s2)
-	
-{
-char *result = malloc(strlen(s1) + strlen(s2) + 1); 
 
-strcpy(result, s1);
-strcat(result, s2);
-return (NULL);
+{
+
+	char *str;
+	int len;
+	int i = 0;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+	while (*(s1 + i))
+		++i;
+	len = i;
+	i = 0;
+	while (*(s2 + i))
+		++i;
+	len += i;
+	str = malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	for (i = 0; *s1; ++i)
+		*(str + i) = *(s1++);
+	for (; i <= len; ++i)
+		*(str + i) = *(s2++);
+	return (str);
+
 }
