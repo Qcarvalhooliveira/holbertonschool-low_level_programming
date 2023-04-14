67a3f1e955c219616126feadb5599a3c0bfbfbf5
root <queisecarvalho@hotmail.com>
2023-04-14 05:26:11 -0700

    Hash task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table to look into
+ * @key: key to look for
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht)
+	{
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index])
+	{
+
+	node = ht->array[index];
+
+	while (node)
+	{
+
+	if (strcmp(node->key, (char *)key) == 0)
+
+	return (node->value);
+																node = node->next;
+	}
+	}
+	}
+	return (NULL);
+}
