c2a0669b27739147a107f3e8170ce0e17fe85053
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-26 16:22:58 +0200

    Binary search algorithm

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array using Linear search.
+ * binary_search - Searches for a value in an array using Binary search.
  * @array: pointer to the first element of the array to search in.
  * @size: the number of elements in array.
  * @value: the value to search for.
