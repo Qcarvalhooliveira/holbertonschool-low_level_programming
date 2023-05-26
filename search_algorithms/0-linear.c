d381a76b203d2c9bd60392b54fb00d5f0cd5c456
Qcarvalhooliveira <queisecarvalho@hotmail.com>
2023-05-26 14:44:26 +0200

    Linear search algorithm

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -19,11 +19,11 @@ int linear_search(int *array, size_t size, int value)
 	for (index = 0; index < size; index++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", index, array[index]);
-	}
+
 	if (array[index] == value)
 	{
 		return (index);
 	}
-
+	}
 	return (-1);
 }
